4aabbae9143137d48f78ccca57f5605268e2aec6
theWarlock <fecypher@gmail.com>
2022-02-22 15:06:44 +0100

    added the chat bot

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,18 +20,12 @@ const Home = () => {
       navigate("/dashboard");
     }
 
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
+    // insert tidio here
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "//code.tidio.co/ddgy1qfhhgt7p01guudw28bqkv3vqvpz.js";
 
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/61cf31ebc82c976b71c444ef/1fo8lm0j3";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
+    document.body.append(script);
   }, []);
 
   return (
@@ -49,3 +43,7 @@ const Home = () => {
 };
 
 export default Home;
+
+{
+  /* <script src="//code.tidio.co/ddgy1qfhhgt7p01guudw28bqkv3vqvpz.js" async></script> */
+}
