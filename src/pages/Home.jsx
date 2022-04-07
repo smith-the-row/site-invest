f26111733b2c09dbd210512dc2161385e42d8bd8
theWarlock <fecypher@gmail.com>
2022-04-07 15:37:10 +0100

    changed chat support

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,17 @@ const Home = () => {
       navigate("/dashboard");
     }
 
-    // insert tidio here
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "//code.tidio.co/mc4zjyzflr75fxtcydcvrco2m8wixpse.js";
-
-    document.body.append(script);
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/624ef599c72df874911dcd0d/1g026uo7k";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
   }, []);
 
   return (
